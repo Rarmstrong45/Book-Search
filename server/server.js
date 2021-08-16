1a179142a1735eb3a3fdaff967c0445e322f60f9
Rarmstrong45 <Ryan_armstrong07@yahoo.com>
2021-08-16 15:48:52 -0500

    another try

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
@@ -45,3 +45,4 @@ db.once('open', () => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
+
